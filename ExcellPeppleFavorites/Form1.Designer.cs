9d747890b95d253f8b506b61a966229e57a7e059
excellpepple <excellpepple@gmail.com>
2025-12-13 19:15:39 -0500

    added form, functionality and resources

---
+++ ExcellPeppleFavorites/Form1.Designer.cs
@@ -0,0 +1,194 @@
+﻿namespace ExcellPeppleFavorites
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.Title_Lbl = new System.Windows.Forms.Label();
+            this.Option1_Img = new System.Windows.Forms.PictureBox();
+            this.Option2_Img = new System.Windows.Forms.PictureBox();
+            this.Option3_Img = new System.Windows.Forms.PictureBox();
+            this.NextBtn = new System.Windows.Forms.Button();
+            this.BackBtn = new System.Windows.Forms.Button();
+            this.ExitBtn = new System.Windows.Forms.Button();
+            this.Elsa_Img = new System.Windows.Forms.PictureBox();
+            this.SelectedLbl = new System.Windows.Forms.Label();
+            this.SelectionLbl = new System.Windows.Forms.Label();
+            ((System.ComponentModel.ISupportInitialize)(this.Option1_Img)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.Option2_Img)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.Option3_Img)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.Elsa_Img)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // Title_Lbl
+            // 
+            this.Title_Lbl.AutoSize = true;
+            this.Title_Lbl.Font = new System.Drawing.Font("Comic Sans MS", 32F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.Title_Lbl.ForeColor = System.Drawing.Color.DarkRed;
+            this.Title_Lbl.Location = new System.Drawing.Point(96, 23);
+            this.Title_Lbl.Name = "Title_Lbl";
+            this.Title_Lbl.Size = new System.Drawing.Size(343, 60);
+            this.Title_Lbl.TabIndex = 0;
+            this.Title_Lbl.Text = "Xl\'s Rock Picks";
+            // 
+            // Option1_Img
+            // 
+            this.Option1_Img.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom)
+                        | System.Windows.Forms.AnchorStyles.Left)
+                        | System.Windows.Forms.AnchorStyles.Right)));
+            this.Option1_Img.Image = ((System.Drawing.Image)(resources.GetObject("Option1_Img.Image")));
+            this.Option1_Img.Location = new System.Drawing.Point(30, 86);
+            this.Option1_Img.Name = "Option1_Img";
+            this.Option1_Img.Size = new System.Drawing.Size(133, 162);
+            this.Option1_Img.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
+            this.Option1_Img.TabIndex = 1;
+            this.Option1_Img.TabStop = false;
+            this.Option1_Img.Click += new System.EventHandler(this.Option1_Img_Click);
+            // 
+            // Option2_Img
+            // 
+            this.Option2_Img.Image = ((System.Drawing.Image)(resources.GetObject("Option2_Img.Image")));
+            this.Option2_Img.Location = new System.Drawing.Point(197, 86);
+            this.Option2_Img.Name = "Option2_Img";
+            this.Option2_Img.Size = new System.Drawing.Size(136, 162);
+            this.Option2_Img.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
+            this.Option2_Img.TabIndex = 2;
+            this.Option2_Img.TabStop = false;
+            this.Option2_Img.Click += new System.EventHandler(this.Option2_Img_Click);
+            // 
+            // Option3_Img
+            // 
+            this.Option3_Img.Image = ((System.Drawing.Image)(resources.GetObject("Option3_Img.Image")));
+            this.Option3_Img.Location = new System.Drawing.Point(360, 86);
+            this.Option3_Img.Name = "Option3_Img";
+            this.Option3_Img.Size = new System.Drawing.Size(139, 162);
+            this.Option3_Img.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
+            this.Option3_Img.TabIndex = 3;
+            this.Option3_Img.TabStop = false;
+            this.Option3_Img.Click += new System.EventHandler(this.Option3_Img_Click);
+            // 
+            // NextBtn
+            // 
+            this.NextBtn.Location = new System.Drawing.Point(30, 308);
+            this.NextBtn.Name = "NextBtn";
+            this.NextBtn.Size = new System.Drawing.Size(75, 23);
+            this.NextBtn.TabIndex = 4;
+            this.NextBtn.Text = "Next";
+            this.NextBtn.UseVisualStyleBackColor = true;
+            this.NextBtn.Click += new System.EventHandler(this.NextBtn_Click);
+            // 
+            // BackBtn
+            // 
+            this.BackBtn.Location = new System.Drawing.Point(197, 308);
+            this.BackBtn.Name = "BackBtn";
+            this.BackBtn.Size = new System.Drawing.Size(75, 23);
+            this.BackBtn.TabIndex = 5;
+            this.BackBtn.Text = "Back";
+            this.BackBtn.UseVisualStyleBackColor = true;
+            this.BackBtn.Click += new System.EventHandler(this.BackBtn_Click);
+            // 
+            // ExitBtn
+            // 
+            this.ExitBtn.Location = new System.Drawing.Point(377, 308);
+            this.ExitBtn.Name = "ExitBtn";
+            this.ExitBtn.Size = new System.Drawing.Size(75, 23);
+            this.ExitBtn.TabIndex = 6;
+            this.ExitBtn.Text = "Exit";
+            this.ExitBtn.UseVisualStyleBackColor = true;
+            this.ExitBtn.Click += new System.EventHandler(this.ExitBtn_Click);
+            // 
+            // Elsa_Img
+            // 
+            this.Elsa_Img.Image = ((System.Drawing.Image)(resources.GetObject("Elsa_Img.Image")));
+            this.Elsa_Img.Location = new System.Drawing.Point(505, 23);
+            this.Elsa_Img.Name = "Elsa_Img";
+            this.Elsa_Img.Size = new System.Drawing.Size(303, 338);
+            this.Elsa_Img.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
+            this.Elsa_Img.TabIndex = 7;
+            this.Elsa_Img.TabStop = false;
+            // 
+            // SelectedLbl
+            // 
+            this.SelectedLbl.AutoSize = true;
+            this.SelectedLbl.Location = new System.Drawing.Point(30, 264);
+            this.SelectedLbl.Name = "SelectedLbl";
+            this.SelectedLbl.Size = new System.Drawing.Size(74, 13);
+            this.SelectedLbl.TabIndex = 8;
+            this.SelectedLbl.Text = "You Selected:";
+            // 
+            // SelectionLbl
+            // 
+            this.SelectionLbl.AutoSize = true;
+            this.SelectionLbl.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.SelectionLbl.Location = new System.Drawing.Point(111, 263);
+            this.SelectionLbl.Name = "SelectionLbl";
+            this.SelectionLbl.Size = new System.Drawing.Size(80, 13);
+            this.SelectionLbl.TabIndex = 9;
+            this.SelectionLbl.Text = "No Selection";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(774, 351);
+            this.Controls.Add(this.SelectionLbl);
+            this.Controls.Add(this.SelectedLbl);
+            this.Controls.Add(this.Elsa_Img);
+            this.Controls.Add(this.ExitBtn);
+            this.Controls.Add(this.BackBtn);
+            this.Controls.Add(this.NextBtn);
+            this.Controls.Add(this.Option3_Img);
+            this.Controls.Add(this.Option2_Img);
+            this.Controls.Add(this.Option1_Img);
+            this.Controls.Add(this.Title_Lbl);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            ((System.ComponentModel.ISupportInitialize)(this.Option1_Img)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.Option2_Img)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.Option3_Img)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.Elsa_Img)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label Title_Lbl;
+        private System.Windows.Forms.PictureBox Option1_Img;
+        private System.Windows.Forms.PictureBox Option2_Img;
+        private System.Windows.Forms.PictureBox Option3_Img;
+        private System.Windows.Forms.Button NextBtn;
+        private System.Windows.Forms.Button BackBtn;
+        private System.Windows.Forms.Button ExitBtn;
+        private System.Windows.Forms.PictureBox Elsa_Img;
+        private System.Windows.Forms.Label SelectedLbl;
+        private System.Windows.Forms.Label SelectionLbl;
+    }
+}
+
---
+++ ExcellPeppleFavorites/Form1.cs
@@ -0,0 +1,85 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+using System.IO;
+
+namespace ExcellPeppleFavorites
+{
+
+    public partial class Form1 : Form
+    {
+        private string[,] page = { // I use this to save myself some keystrokes :)
+            { "Warrant", "warrant.jpg"}, {" Skid", "skid.jpg"}, {"Poison", "poison.jpg"},
+            {"Motley", "motley.jpg"}, {"Jovi", "jovi.jpg"}, {"GNR", "gnr.jpg"}
+        };
+        private string rootDir = Path.GetFullPath( //finds the root folder for this project 
+            Path.Combine(Application.StartupPath, @"..\..\..\")
+        );
+        private int currentPage = 0; // my idea of pagination: it tracks what options to display
+        public Form1()
+        {
+            InitializeComponent();
+            UpdatePage();
+
+        }
+        private void UpdatePage() {
+            /*
+             * Updates the page by changing the source of the picture boxes and disables or enables the buttons
+             **/
+            Option1_Img.Image = Image.FromFile(Path.Combine(rootDir, page[currentPage, 1]));
+            Option2_Img.Image = Image.FromFile(Path.Combine(rootDir, page[currentPage + 1, 1]));
+            Option3_Img.Image = Image.FromFile(Path.Combine(rootDir, page[currentPage + 2, 1]));
+            SelectionLbl.Text = "Not Selected";
+            if (currentPage == 1)
+            {
+                NextBtn.Enabled = false;
+                BackBtn.Enabled = true;
+            }
+            if (currentPage == 0)
+            {
+                NextBtn.Enabled = true;
+                BackBtn.Enabled = false;
+            }
+        }
+
+        private void Option1_Img_Click(object sender, EventArgs e)
+        {
+            SelectionLbl.Text = ""+page[currentPage, 0];
+        }
+
+        private void Option3_Img_Click(object sender, EventArgs e)
+        {
+            SelectionLbl.Text = "" + page[currentPage +2, 0];
+        }
+
+        private void NextBtn_Click(object sender, EventArgs e)
+        {
+            currentPage++;
+            UpdatePage();
+
+        }
+
+        private void BackBtn_Click(object sender, EventArgs e)
+        {
+            currentPage--;
+            UpdatePage();
+        }
+
+        private void ExitBtn_Click(object sender, EventArgs e)
+        {
+            Application.Exit();
+        }
+
+        private void Option2_Img_Click(object sender, EventArgs e)
+        {
+            SelectionLbl.Text = "" + page[currentPage + 1, 0];
+        }
+
+        
+    }
+}
